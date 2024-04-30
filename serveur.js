4b7e1d412767382fde4c39ec2feaca644bc9c15a
unknown <elhadjoumartall10@gmail.com>
2024-04-30 17:38:17 +0200

    Definir le port et le modele ejs

--- serveur.js
+++ serveur.js
@@ -1,7 +1,12 @@
+// Definir les dependances
 const express = require("express");
 const app = express();
 
+// Definier le modele ejs template 
+app.set("view engine", "ejs");
 
+
+// Definir le port du serveur
 app.listen(3000, () => {
     console.log("Mon serveur est lance au port 3000");
 })
